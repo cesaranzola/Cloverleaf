1038a203ffdd0401fc7f76e7aa83a4e068a8c535
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-21 20:53:37 -0500

    Add port listener

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const methodOverRide = require('method-override');
+const res = require('express/lib/response');
+
+//==================Port=============================
+app.listen(3000, () => {
+	console.log('Listening on the port 3000...');
+});
