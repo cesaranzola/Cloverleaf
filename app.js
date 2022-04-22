3bb76946e58c61a3a41f2ffa512dd094880d442f
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-21 20:55:38 -0500

    Add index route

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const path = require('path');
 const methodOverRide = require('method-override');
 const res = require('express/lib/response');
 
+//==================Index Route======================
+app.set('/', (req, res) => {
+	res.send('Hello, from Cloverleaf');
+});
+
 //==================Port=============================
 app.listen(3000, () => {
-	console.log('Listening on the port 3000...');
+	console.log('Serving on the port 3000...');
 });
