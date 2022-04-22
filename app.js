597ae75eff816fbd293e05b6ea6dc5837257e810
Cesar Anzola <cesar.rodriguez.anzola@protonmail.com>
2022-04-21 22:14:51 -0500

    Added models and campground

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, '/views'));
 
 //==================Index Route======================
 app.get('/', (req, res) => {
-	res.send('Hello, from Cloverleaf');
+	res.render('home');
 });
 
 //==================Port=============================
